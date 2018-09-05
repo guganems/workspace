c998b00e972436e79afe7b801203c8d8117d29f7
gnems <g_nemsveridze@cu.edu.ge>
2018-09-05 15:34:34 +0400

    added timer item in drawable

--- TimerTutorial/app/src/main/java/com/example/android/timertutorial/TimerActivity.kt
+++ TimerTutorial/app/src/main/java/com/example/android/timertutorial/TimerActivity.kt
@@ -15,10 +15,6 @@ class TimerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_timer)
         setSupportActionBar(toolbar)
 
-        //fab.setOnClickListener { view ->
-          //  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            //        .setAction("Action", null).show()
-      //  }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
