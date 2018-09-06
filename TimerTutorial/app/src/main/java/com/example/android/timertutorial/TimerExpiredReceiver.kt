15bee9ada076964d52ee8af7ff7e005613989c67
gnems <g_nemsveridze@cu.edu.ge>
2018-09-06 11:59:32 +0400

    added notifications

--- TimerTutorial/app/src/main/java/com/example/android/timertutorial/TimerExpiredReceiver.kt
+++ TimerTutorial/app/src/main/java/com/example/android/timertutorial/TimerExpiredReceiver.kt
@@ -3,12 +3,13 @@ package com.example.android.timertutorial
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.android.timertutorial.util.NotificationUtil
 import com.example.android.timertutorial.util.PrefUtil
 
 class TimerExpiredReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        //TODO: show notification
+        NotificationUtil.showTimerExpired(context)
 
         PrefUtil.setTimerState(TimerActivity.TimerState.Stopped, context)
         PrefUtil.setAlarmSetTime(0, context)
