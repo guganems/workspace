9fba57c9673177baa496ca9ab18ec7c43ff1df92
gnems <g_nemsveridze@cu.edu.ge>
2018-09-06 12:36:49 +0400

    Working with Anko library

---
+++ AnkoTutorial/app/src/main/java/com/example/android/ankotutorial/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.android.ankotutorial
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.frameLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
