aca9c38fdc7e60cff9f45f128838faf4778868de
gnems <g_nemsveridze@cu.edu.ge>
2018-09-07 15:10:53 +0400

    working on my account fragment

--- FireMessage/app/src/main/java/com/example/android/firemessage/fragment/MyAccountFragment.kt
+++ FireMessage/app/src/main/java/com/example/android/firemessage/fragment/MyAccountFragment.kt
@@ -9,16 +9,12 @@ import android.view.ViewGroup
 
 import com.example.android.firemessage.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
+
 class MyAccountFragment : Fragment() {
+
+    private val RC_SELECT_IMAGE = 2
+    private lateinit var selectedImageBytes: ByteArray
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
