decae8e50471c6cd6efd8d6d2f51db170dabfaf8
gnems <g_nemsveridze@cu.edu.ge>
2018-09-07 16:59:15 +0400

    added empty framelayout in my account fragment

--- FireMessage/app/src/main/java/com/example/android/firemessage/MainActivity.kt
+++ FireMessage/app/src/main/java/com/example/android/firemessage/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.android.firemessage
 
+import android.app.Fragment
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatDialogFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,13 +22,18 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.navigation_my_account ->{
-                    //TODO: show my account fragment
                     true
                 }
                 else -> {
                     false
                 }
             }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace()
         }
     }
 }
