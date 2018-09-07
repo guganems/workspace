3e5b64242cefa81fd7db9305e695078579e347fd
gnems <g_nemsveridze@cu.edu.ge>
2018-09-07 09:53:21 +0400

    Added Activity to determine which activity have to start

--- FireMessage/app/src/main/java/com/example/android/firemessage/SignInActivity.kt
+++ FireMessage/app/src/main/java/com/example/android/firemessage/SignInActivity.kt
@@ -1,12 +1,65 @@
 package com.example.android.firemessage
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.design.snackbar
 
 class SignInActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 1
+
+    private val signInProviders =
+            listOf(AuthUI.IdpConfig.EmailBuilder()
+                    .setAllowNewAccounts(true)
+                    .setRequireName(true)
+                    .build())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        account_sign_in.setOnClickListener {
+            val intent = AuthUI.getInstance().createSignInIntentBuilder()
+                    .setAvailableProviders(signInProviders)
+                    .setLogo(R.drawable.ic_fire_emoji)
+                    .build()
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val responce = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK){
+                val progressDialog = indeterminateProgressDialog("Setting up your account")
+                //TODO: Initialize current user in firestore
+                startActivity(intentFor<MainActivity>().newTask().clearTask())
+                progressDialog.dismiss()
+            }
+            else if (resultCode == Activity.RESULT_CANCELED){
+                if (responce == null){
+                    return
+                }
+
+                when(responce.error?.errorCode){
+                    ErrorCodes.NO_NETWORK ->{
+                        snackbar(constraint_layout, "No Network")
+                    }
+                    ErrorCodes.UNKNOWN_ERROR ->{
+                        snackbar(constraint_layout, "Unknown Network")
+                    }
+                }
+            }
+        }
     }
 }
