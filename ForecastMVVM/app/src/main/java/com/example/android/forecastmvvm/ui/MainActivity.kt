cc6ed13e1a150db0b3a127ddbd08f72d005ca8d1
gnems <g_nemsveridze@cu.edu.ge>
2018-10-31 06:00:52 +0400

    UI package created

--- ForecastMVVM/app/src/main/java/com/example/android/forecastmvvm/ui/MainActivity.kt
+++ ForecastMVVM/app/src/main/java/com/example/android/forecastmvvm/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.android.forecastmvvm
+package com.example.android.forecastmvvm.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.forecastmvvm.R
 
 class MainActivity : AppCompatActivity() {
 
