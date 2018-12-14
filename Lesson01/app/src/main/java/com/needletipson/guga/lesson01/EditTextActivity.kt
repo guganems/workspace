70b9298812c08eb9b07cb64815c03f9b23bd9f28
gnems <g_nemsveridze@cu.edu.ge>
2018-12-14 07:14:01 +0400

    Add textChangedListener

--- Lesson01/app/src/main/java/com/needletipson/guga/lesson01/EditTextActivity.kt
+++ Lesson01/app/src/main/java/com/needletipson/guga/lesson01/EditTextActivity.kt
@@ -2,6 +2,8 @@ package com.needletipson.guga.lesson01
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -23,5 +25,19 @@ class EditTextActivity : AppCompatActivity() {
         copy.setOnClickListener{
             result.text = editText.text
         }
+
+        editText.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                result.text = p0
+            }
+        })
     }
 }
