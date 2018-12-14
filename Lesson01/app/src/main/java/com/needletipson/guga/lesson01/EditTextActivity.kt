51468c53b3ec0929e3998400a20bc024255c0705
gnems <g_nemsveridze@cu.edu.ge>
2018-12-14 06:55:27 +0400

    Using editText

---
+++ Lesson01/app/src/main/java/com/needletipson/guga/lesson01/EditTextActivity.kt
@@ -0,0 +1,27 @@
+package com.needletipson.guga.lesson01
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class EditTextActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+    private lateinit var result: TextView
+    private lateinit var copy : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_text)
+
+        editText = findViewById(R.id.edittext)
+        result = findViewById(R.id.tv_result)
+        copy = findViewById(R.id.btn_copy)
+
+        copy.setOnClickListener{
+            result.text = editText.text
+        }
+    }
+}
