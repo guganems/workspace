32941894f8e67dcfe8834c984b362be0b98071a0
gnems <g_nemsveridze@cu.edu.ge>
2018-12-14 15:51:30 +0400

    RecyclerView

--- RecyclerView/app/src/main/java/com/needletipson/guga/recyclerview/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/needletipson/guga/recyclerview/MainActivity.kt
@@ -1,29 +1,53 @@
 package com.needletipson.guga.recyclerview
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.country_child.view.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var country_list: RecyclerView
-    var countries: MutableList<String> = ArrayList()
+    private lateinit var countryList: RecyclerView
+    private var countries: MutableList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        country_list = findViewById(R.id.country_list) as RecyclerView
+        loadData()
+        countryList = findViewById(R.id.country_list)
+        //country_list.layoutManager = LinearLayoutManager(this)
+        countryList.layoutManager = GridLayoutManager(this, 2)
+        countryList.adapter = CountryAdapter(countries, this)
     }
 
-    class CountryAdapter: RecyclerView.Adapter<>(){
-        class ViewHolder(v: View): RecyclerView.ViewHolder(){
-            
+    class CountryAdapter(items: List<String>, ctx: Context): RecyclerView.Adapter<CountryAdapter.ViewHolder>(){
+
+        var list = items
+        var context = ctx
+
+        class ViewHolder(v: View): RecyclerView.ViewHolder(v){
+            val name = v.country_name!!
+        }
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+            return ViewHolder(LayoutInflater.from(context).inflate(R.layout.country_child, p0, false))
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+            p0.name.text = list[p1]
         }
     }
 
-    fun loadData(){
+    private fun loadData(){
         countries.add("Georgia")
         countries.add("USA")
         countries.add("Great Britain")
