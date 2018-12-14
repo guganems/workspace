50f1171235c2e22f35e981f2377729b69c0f5118
gnems <g_nemsveridze@cu.edu.ge>
2018-12-14 07:18:49 +0400

    EditText

--- Lesson01/app/src/main/java/com/needletipson/guga/lesson01/EditTextActivity.kt
+++ Lesson01/app/src/main/java/com/needletipson/guga/lesson01/EditTextActivity.kt
@@ -32,7 +32,7 @@ class EditTextActivity : AppCompatActivity() {
             }
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                
+
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
