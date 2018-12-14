35d343a3effb7ae7c172cc0cd8dc653cc2973b1d
gnems <g_nemsveridze@cu.edu.ge>
2018-12-14 16:55:11 +0400

    Fragments

--- Fragment/app/src/main/java/com/needletipson/guga/fragment/MainActivity.kt
+++ Fragment/app/src/main/java/com/needletipson/guga/fragment/MainActivity.kt
@@ -2,11 +2,43 @@ package com.needletipson.guga.fragment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var isFragmentOneLoaded = true
+    private val manager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val change = findViewById<Button>(R.id.btn_change)
+
+        showFragmentOne()
+        change.setOnClickListener{
+            if(isFragmentOneLoaded)
+                showFragmentTwo()
+            else
+                showFragmentOne()
+        }
+    }
+
+    private fun showFragmentOne(){
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentOne()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = true
+    }
+
+    private fun showFragmentTwo(){
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentTwo()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = false
     }
 }
