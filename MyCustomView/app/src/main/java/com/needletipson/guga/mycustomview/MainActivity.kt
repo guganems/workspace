079f1e16fecd2c180ef427e680a17972ae9df482
gnems <g_nemsveridze@cu.edu.ge>
2018-12-29 15:14:26 +0400

    Custom View

--- MyCustomView/app/src/main/java/com/needletipson/guga/mycustomview/MainActivity.kt
+++ MyCustomView/app/src/main/java/com/needletipson/guga/mycustomview/MainActivity.kt
@@ -2,11 +2,27 @@ package com.needletipson.guga.mycustomview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list : MutableList<String> = ArrayList()
+        list.add("Kotlin")
+        list.add("Java")
+        list.add("C++")
+        list.add("HTML")
+        list.add("JavaScript")
+        list.add("PHP")
+
+        customview.setData(list)
+        customview.setTitle("Languages")
+        submit.setOnClickListener {
+            Toast.makeText(this, "Selected Datas: " + customview.getSelectedData().toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
