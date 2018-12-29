b05891eb38a6d3e1944d912a04207dc7dec29398
gnems <g_nemsveridze@cu.edu.ge>
2018-12-29 14:04:08 +0400

    Working on Custom View

---
+++ MyCustomView/app/src/main/java/com/needletipson/guga/mycustomview/MyCustomView.kt
@@ -0,0 +1,11 @@
+package com.needletipson.guga.mycustomview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class MyCustomView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+    init {
+        
+    }
+}
