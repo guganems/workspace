9f49c20be142da019529dafa87e5e0f6031b1433
gnems <g_nemsveridze@cu.edu.ge>
2018-12-31 13:36:39 +0400

    ProgressDialog

--- Progress2/app/src/main/java/com/needletipson/guga/progress2/MainActivity.kt
+++ Progress2/app/src/main/java/com/needletipson/guga/progress2/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.needletipson.guga.progress2
 
+import android.app.AlertDialog
 import android.app.ProgressDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,11 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val progressDialog = ProgressDialog(this)
-        progressDialog.setMessage("Downloading...")
-        progressDialog.setCancelable(false)
-        progressDialog.show()
+//        val progressDialog = ProgressDialog(this)
+//        progressDialog.setMessage("Downloading...")
+//        progressDialog.setCancelable(false)
+//        progressDialog.show()
 
-        Handler().postDelayed({progressDialog.dismiss()}, 5000)
+        val builder = AlertDialog.Builder(this)
+        val dialogView = layoutInflater.inflate(R.layout.progress_dialog, null)
+        val message = dialogView.findViewById<TextView>(R.id.message)
+        message.text = "Downloading..."
+        builder.setView(dialogView)
+        builder.setCancelable(false)
+        val dialog = builder.create()
+        dialog.show()
+
+        Handler().postDelayed({dialog.dismiss()}, 5000)
     }
 }
