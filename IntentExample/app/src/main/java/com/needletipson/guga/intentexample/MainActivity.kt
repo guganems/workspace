ce7c1f851c6ceb0c168aebc2f26d2a68e3afa40c
gnems <g_nemsveridze@cu.edu.ge>
2019-01-02 11:28:36 +0400

    Intents

---
+++ IntentExample/app/src/main/java/com/needletipson/guga/intentexample/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.needletipson.guga.intentexample
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val context = this
+        btn_explicit.setOnClickListener {
+            val intent = Intent(context, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        btn_implicit.setOnClickListener {
+            var intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, "Guga Nemsitsveridze")
+            intent.type = "text/plain"
+            startActivity(intent)
+        }
+    }
+}
