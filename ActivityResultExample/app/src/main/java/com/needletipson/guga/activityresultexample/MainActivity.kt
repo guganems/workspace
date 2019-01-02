0eae853d85bd634ca6e814c86d13a85f1bed1c7f
gnems <g_nemsveridze@cu.edu.ge>
2019-01-02 13:22:25 +0400

    Permissions

--- ActivityResultExample/app/src/main/java/com/needletipson/guga/activityresultexample/MainActivity.kt
+++ ActivityResultExample/app/src/main/java/com/needletipson/guga/activityresultexample/MainActivity.kt
@@ -3,25 +3,49 @@ package com.needletipson.guga.activityresultexample
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.vicky.permission.PathUtils
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.FileInputStream
+import java.util.jar.Manifest
 
-const val FILE_PICKER_ID = 12
-
+private const val FILE_PICKER_ID = 12
+private const val PERMISSION_REQUEST = 10
 class MainActivity : AppCompatActivity() {
 
-    lateinit var context: Context
+    private var permissions = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    private lateinit var context: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         context = this
-        btn_read.setOnClickListener({
-            var intent = Intent(Intent.ACTION_GET_CONTENT)
+
+        btn_request_permissions.setOnClickListener({
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkPermission(context, permissions)) {
+                    Toast.makeText(context, "Permission are already provided.", Toast.LENGTH_SHORT).show()
+                } else {
+                    requestPermissions(permissions, PERMISSION_REQUEST)
+                }
+            }
+            else{
+                Toast.makeText(context, "Permission are already provided.", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        btn_camera.setOnClickListener({
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            startActivity(intent)
+        })
+
+        btn_file.setOnClickListener({
+            intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "*/*"
             startActivityForResult(intent, FILE_PICKER_ID)
         })
@@ -32,8 +56,39 @@ class MainActivity : AppCompatActivity() {
 
         if (requestCode == FILE_PICKER_ID && resultCode == Activity.RESULT_OK){
             val dest = File(PathUtils.getPath(context, data!!.data))
-            val readData = FileInputStream(dest).bufferedReader().use { it.readText() }
-            tv_read_data.text = readData
+            val inputAsString = FileInputStream(dest).bufferedReader().use { it.readText() }
+            btn_file.text = inputAsString
+        }
+    }
+
+    fun checkPermission(context : Context, permissionArray : Array<String>) : Boolean{
+        var allSuccess = true
+        for (i in permissionArray.indices){
+            if (checkCallingOrSelfPermission(permissionArray[i]) == PackageManager.PERMISSION_DENIED)
+                allSuccess = false
+        }
+        return allSuccess
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST){
+            var allSuccess = true
+            for (i in permissions.indices){
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED){
+                    allSuccess = false
+                    var requestAgain = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(permissions[i])
+                    if (requestAgain){
+                        Toast.makeText(context, "Permission denied", Toast.LENGTH_SHORT).show()
+                    }
+                    else{
+                        Toast.makeText(context, "Go to settings and enable the permission", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            if (allSuccess){
+                Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
