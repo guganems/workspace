119e1796956a2f9c2bbd40a14e3802d52dc1f4ed
gnems <g_nemsveridze@cu.edu.ge>
2019-01-02 19:33:05 +0400

    Working on broadcast

---
+++ BroadcastReceiverExample/app/src/main/java/com/needletipson/guga/broadcastreceiverexample/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.needletipson.guga.broadcastreceiverexample
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var receiver: BroadcastReceiver
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+        receiver = object : BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                Toast.makeText(p0, intent?.action, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        registerReceiver(receiver, filter)
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
+    }
+}
