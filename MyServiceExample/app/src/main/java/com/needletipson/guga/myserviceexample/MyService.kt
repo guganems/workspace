5e57652e0cf376276acbc18638e7dac979726172
gnems <g_nemsveridze@cu.edu.ge>
2019-01-04 12:01:43 +0400

    Create intent to start service

---
+++ MyServiceExample/app/src/main/java/com/needletipson/guga/myserviceexample/MyService.kt
@@ -0,0 +1,40 @@
+package com.needletipson.guga.myserviceexample
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyService : Service() {
+
+    val TAG = "MyService"
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        ShowLog("onCreate")
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        ShowLog("onStartCommand")
+
+        for (i in 1..10){
+            ShowLog("Service Doing something " + i.toString())
+            Thread.sleep(1000)
+        }
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        ShowLog("onDestroy")
+        super.onDestroy()
+    }
+
+    fun ShowLog(message: String){
+        Log.d(TAG, message)
+    }
+}
