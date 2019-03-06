42c40721ec401030c380d9c873725ec0486fe48f
guganems <g_nemsveridze@cu.edu.ge>
2019-03-06 12:39:30 +0400

    Working on enumerations

--- cpp/EnumSample/EnumSample.cpp
+++ cpp/EnumSample/EnumSample.cpp
@@ -3,15 +3,6 @@
 #include <string>
 #include "../pchar.h"
 
-enum class Color {
-	Red,
-	Orange,
-	Yellow,
-	Blue,
-	Indigo,
-	Violet
-};
-
 enum Flavor : unsigned short int {
 	Vanilla,
 	Chocolate,
@@ -20,6 +11,15 @@ enum Flavor : unsigned short int {
 };
 
 int _pmain(int, _pchar[]) {
+	enum class Color {
+		Red,
+		Orange,
+		Yellow,
+		Blue,
+		Indigo,
+		Violet
+	};
+	
 	Flavor f = Vanilla;
 	f = Mint;
 	
@@ -31,8 +31,26 @@ int _pmain(int, _pchar[]) {
 	switch (c) {
 		case Color::Red: color = L"Red"; break;
 		case Color::Orange: color = L"Orange"; break;
-		case Color::Yellow: color = 
+		case Color::Yellow: color = L"Yellow"; break;
+		case Color::Blue: color = L"Blue"; break;
+		case Color::Indigo: color = L"Indigo"; break;
+		case Color::Violet: color = L"Violet"; break;
+		default: color = L"Unknown"; break;
 	}
+	
+	switch(f) {
+		case Vanilla: flavor = L"Vanilla"; break;
+		case Chocolate: flavor = L"Chocolate"; break;
+		case Strawberry: flavor = L"Strawberry"; break;
+		case Mint: flavor = L"Mint"; break;
+		default: break;
+	}
+	
+	std::wcout << L"Flavor is " << flavor.c_str() << L" (" << f << L"). Color is " << color.c_str() << L"( " << 
+		static cast<int>(c) << L"). " << std::endl << L"The size of flavor is " << sizeof(Flavor) << L"." << std::endl <<
+		L"The size of Color is " << sizeof(Color) << L"." std::endl;
+		
+	return 0;
 }
 
 
---
+++ cpp/EnumSample/Main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Guga on 06.03.2019.
+//
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "GUGA";
+}
