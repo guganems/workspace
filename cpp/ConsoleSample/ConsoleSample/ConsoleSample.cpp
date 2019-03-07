783aeea528933842783f5a1e8a83548605685393
guganems <g_nemsveridze@cu.edu.ge>
2019-03-07 15:05:24 +0400

    Working on wcout

--- cpp/ConsoleSample/ConsoleSample/ConsoleSample.cpp
+++ cpp/ConsoleSample/ConsoleSample/ConsoleSample.cpp
@@ -10,6 +10,10 @@ struct Color {
 	float A(void) const { return ARGB[0]; }
 	void R(float value) { ARGB[1] = value; }
 	float R(void) const { return ARGB[1]; }
+	void G(float value) { ARGB[2] = value; }
+	float G(void) const { return ARGB[2]; }
+	void B(float value) { ARGB[3] = value; }
+	float B(void) const { return ARGB[3]; }
 };
 
 int main()
