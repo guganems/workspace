b9386c72f992581da67e23d158821a42f8aae1e6
guganems <g_nemsveridze@cu.edu.ge>
2019-03-18 13:25:31 +0400

    Add FunctionsSample

--- cpp/FunctionsSample/FunctionsSample/FunctionsSample.cpp
+++ cpp/FunctionsSample/FunctionsSample/FunctionsSample.cpp
@@ -1,21 +1,25 @@
-// FunctionsSample.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
-#include <iostream>
+#include "Utility.h"
+#include "pchar.h"
 
-int main()
+using namespace Utility;
+
+int _pmain(int, _pchar*[])
 {
-    std::cout << "Hello World!\n"; 
+	int i1 = 3;
+	int i2 = 4;
+
+	long long ll1 = 6;
+	long long ll2 = 7;
+
+	bool b1 = IsEven(i1);
+
+	PrintBool(b1);
+
+	PrintIsEvenResult(i1);
+	PrintIsEvenResult(i2);
+	PrintIsEvenResult(ll1);
+	PrintIsEvenResult(ll2);
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
