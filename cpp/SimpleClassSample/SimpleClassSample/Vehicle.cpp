ca56c41338a82fe9269ed65e3605d349762b6276
guganems <g_nemsveridze@cu.edu.ge>
2019-03-27 15:06:49 +0400

    Add Vehicle source

---
+++ cpp/SimpleClassSample/SimpleClassSample/Vehicle.cpp
@@ -0,0 +1,17 @@
+#include "Vehicle.h"
+#include "VehicleCondition.h"
+
+using namespace Inventory;
+using namespace std;
+
+Vehicle::Vehicle(VehicleCondition condition, double pricePaid) : m_condition(condition), m_basis(pricePaid) {
+
+}
+
+Vehicle::~Vehicle(void) {
+
+}
+
+void Vehicle::setVehicleCondition(VehicleCondition condition) {
+	m_condition = condition;
+}
