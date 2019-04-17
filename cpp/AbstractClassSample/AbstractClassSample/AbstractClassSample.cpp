a43f208feb89ef9a6955a59d1b7ff2835e1cf2ca
guganems <g_nemsveridze@cu.edu.ge>
2019-04-17 16:19:34 +0400

    Add AbstractClassSample.cpp

--- cpp/AbstractClassSample/AbstractClassSample/AbstractClassSample.cpp
+++ cpp/AbstractClassSample/AbstractClassSample/AbstractClassSample.cpp
@@ -1,20 +1,18 @@
-// AbstractClassSample.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "IWriteData.h"
+#include "ConsoleWriteData.h"
+#include "pchar.h"
 
-#include <iostream>
-
-int main()
+int _pmain(int, _pchar* [])
 {
-    std::cout << "Hello World!\n"; 
+	ConsoleWriteData cwd = ConsoleWriteData();
+
+	IWriteData& r_iwd = cwd;
+
+	IWriteData* p_iwd = &cwd;
+
+	cwd.WriteLine(10);
+	r_iwd.WriteLine(14.6);
+	p_iwd->WriteLine(L"Hello, Abstract World!");
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
