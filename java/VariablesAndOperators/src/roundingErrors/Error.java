fe60a66aba8c5ed64f3d5abc9708f2769b25ad0b
guganems <g_nemsveridze@cu.edu.ge>
2019-07-01 17:31:01 +0400

    Add rounding errors

---
+++ java/VariablesAndOperators/src/roundingErrors/Error.java
@@ -0,0 +1,8 @@
+package roundingErrors;
+
+public class Error {
+    public static void main(String[] args) {
+        System.out.println(0.1 * 10);
+        System.out.println(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
+    }
+}
