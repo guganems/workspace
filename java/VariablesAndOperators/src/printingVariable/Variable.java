4a932159b86d74f6dfc8a9f7dcf16d2312729c08
guganems <g_nemsveridze@cu.edu.ge>
2019-07-01 17:12:46 +0400

    Printing variables

---
+++ java/VariablesAndOperators/src/printingVariable/Variable.java
@@ -0,0 +1,13 @@
+package printingVariable;
+
+public class Variable {
+    public static void main(String[] args) {
+        int hour = 11;
+        int minute = 59;
+        System.out.print("The current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.print(minute);
+        System.out.println(".");
+    }
+}
