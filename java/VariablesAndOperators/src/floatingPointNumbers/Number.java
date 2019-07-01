ba951b67c355be5bde7646d5ea6d3be0f96f1dbb
guganems <g_nemsveridze@cu.edu.ge>
2019-07-01 17:25:35 +0400

    Add floating-point numbers

---
+++ java/VariablesAndOperators/src/floatingPointNumbers/Number.java
@@ -0,0 +1,9 @@
+package floatingPointNumbers;
+
+public class Number {
+    public static void main(String[] args) {
+        double minute = 59.0;
+        System.out.print("Fraction of the hour that has passed: ");
+        System.out.println(minute / 60.0);
+    }
+}
