5aae5948a9158f9b86dd31c083e6c96b07fc80c3
guganems <g_nemsveridze@cu.edu.ge>
2019-07-01 16:32:49 +0400

    Add backslash escaping

--- java/TheWayOfTheProgram/src/escapeSequences/Hello.java
+++ java/TheWayOfTheProgram/src/escapeSequences/Hello.java
@@ -3,5 +3,6 @@ package escapeSequences;
 public class Hello {
     public static void main(String[] args) {
         System.out.print("Hello!\nHow are you doing?\n");
+        System.out.println("She said \"Hello\" to me.");
     }
 }
