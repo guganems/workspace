7a06ec09ba4183ba5c6d6942dd0bbb5986e90f19
guganems <g_nemsveridze@cu.edu.ge>
2019-07-01 17:21:32 +0400

    Add percent

---
+++ java/VariablesAndOperators/src/arithmeticOperations/Percent.java
@@ -0,0 +1,10 @@
+package arithmeticOperations;
+
+public class Percent {
+    public static void main(String[] args) {
+        int hour = 11;
+        int minute = 59;
+        System.out.print("Percent of the hour that has passed: ");
+        System.out.println(minute * 100 / 60);
+    }
+}
