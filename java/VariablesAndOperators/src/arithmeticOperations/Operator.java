d87754942c065a393fff1202b5846ac661e2084b
guganems <g_nemsveridze@cu.edu.ge>
2019-07-01 17:17:58 +0400

    Arithmetic operators

---
+++ java/VariablesAndOperators/src/arithmeticOperations/Operator.java
@@ -0,0 +1,10 @@
+package arithmeticOperations;
+
+public class Operator {
+    public static void main(String[] args) {
+        int hour = 11;
+        int minute = 59;
+        System.out.print("Number of minutes since midnight: ");
+        System.out.println(hour * 60 + minute);
+    }
+}
