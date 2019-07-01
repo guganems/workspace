23445afb8d98cf8a09c496ef5b1456ce229803fa
guganems <g_nemsveridze@cu.edu.ge>
2019-07-01 16:43:10 +0400

    Add exerc2

---
+++ java/TheWayOfTheProgram/src/exercises/exerc2.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class exerc2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // comment
+        System.out.println("How are you?");
+    }
+}
