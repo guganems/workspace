abd9dac111100632b5030cfd82f4356a4fcbbccb
guganems <g_nemsveridze@cu.edu.ge>
2019-07-02 11:23:29 +0400

    Add date

---
+++ java/VariablesAndOperators/src/exercises/Date.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class Date {
+    public static void main(String[] args) {
+        String day = "Tuesday";
+        String date = "2nd";
+        String month = "July";
+        String year = "2019";
+        System.out.println("American format:");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+        System.out.println("European format:");
+        System.out.println(day + " " + date + " " + month + " " + year);
+    }
+}
