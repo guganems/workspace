3c4ea3f2215380bf1397c8b7abe78e005d0388d1
guganems <g_nemsveridze@cu.edu.ge>
2019-07-02 13:44:32 +0400

    Add formatted

--- java/InputAndOutput/src/formattingOutput/Output.java
+++ java/InputAndOutput/src/formattingOutput/Output.java
@@ -2,6 +2,10 @@ package formattingOutput;
 
 public class Output {
     public static void main(String[] args) {
+        final double CM_PER_INCH = 2.54;
         System.out.println(4.0 / 3.0);
+        int inch = 100;
+        double cm = inch * CM_PER_INCH;
+        System.out.printf("%d in = %f cm\n", inch, cm);
     }
 }
