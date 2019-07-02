7beb53a0a8eea72395e7a577188e566f24869e00
guganems <g_nemsveridze@cu.edu.ge>
2019-07-02 16:38:05 +0400

    Add exerc2

---
+++ java/VoidMethods/src/exercises/Exerc2.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class Exerc2 {
+    private static void zool(int number, String name, String street) {
+        System.out.println(number);
+        System.out.println(name);
+        System.out.println(street);
+    }
+
+    public static void main(String[] args) {
+        zool(11, "Cristiano", "Utskheri");
+    }
+}
