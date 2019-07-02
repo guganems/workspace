2339e738e245cffffbf4383d81ddf05f5e87eb2d
guganems <g_nemsveridze@cu.edu.ge>
2019-07-02 16:15:33 +0400

    Add printTime

---
+++ java/VoidMethods/src/stackDiagrams/PrintTime.java
@@ -0,0 +1,15 @@
+package stackDiagrams;
+
+public class PrintTime {
+    private static void printTime(int hour, int minute) {
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.println(minute);
+    }
+
+    public static void main(String[] args) {
+        int hour = 11;
+        int minute = 59;
+        printTime(hour, minute);
+    }
+}
