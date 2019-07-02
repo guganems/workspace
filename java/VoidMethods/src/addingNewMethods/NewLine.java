dd9c18b3c2edc119d8253e07aa6413ea629c1a92
guganems <g_nemsveridze@cu.edu.ge>
2019-07-02 15:51:09 +0400

    Add newLine

---
+++ java/VoidMethods/src/addingNewMethods/NewLine.java
@@ -0,0 +1,13 @@
+package addingNewMethods;
+
+public class NewLine {
+    public static void newLine() {
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("First Line.");
+        newLine();
+        System.out.println("Second Line.");
+    }
+}
