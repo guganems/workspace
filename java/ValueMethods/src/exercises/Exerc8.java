62b696beb604744840f3d81f611cb1b83aa86628
guganems <g_nemsveridze@cu.edu.ge>
2019-07-03 16:54:43 +0400

    add exerc8

---
+++ java/ValueMethods/src/exercises/Exerc8.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class Exerc8 {
+    public static void main(String[] args) {
+        System.out.println(ack(3, 12));
+    }
+
+    private static int ack(int m, int n) {
+        if (m == 0) return n + 1;
+        if (m > 0 && n == 0) return ack(m-1, 1);
+        if (m > 0 && n > 0) return ack(m-1, ack(m, n-1));
+        else return 0;
+    }
+}
