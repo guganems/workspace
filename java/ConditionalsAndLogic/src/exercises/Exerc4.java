5b0d491058426af1ae9cb420e2c235c44add4e4c
guganems <g_nemsveridze@cu.edu.ge>
2019-07-03 13:29:19 +0400

    Add fermat

--- java/ConditionalsAndLogic/src/exercises/Exerc4.java
+++ java/ConditionalsAndLogic/src/exercises/Exerc4.java
@@ -1,7 +1,27 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class Exerc4 {
     public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        int n;
+        Scanner input = new Scanner(System.in);
+        a = input.nextInt();
+        b = input.nextInt();
+        c = input.nextInt();
+        n = input.nextInt();
+        checkFermat(a, b, c, n);
+    }
 
+    private static void checkFermat(int a, int b, int c, int n) {
+        if (Math.pow(a, n) + Math.pow(b, n) == Math.pow(c, n)) {
+            System.out.println("Holy smokes, Fermat was wrong!");
+        }
+        else  {
+            System.out.println("No, that doesn't work");
+        }
     }
 }
