dac293d808d85f8709a55280da820d1f4072212d
guganems <g_nemsveridze@cu.edu.ge>
2019-07-04 13:03:57 +0400

    Add dsa

--- java/Loops/src/encapsulationAndGeneralization/Encapsulation.java
+++ java/Loops/src/encapsulationAndGeneralization/Encapsulation.java
@@ -2,9 +2,13 @@ package encapsulationAndGeneralization;
 
 public class Encapsulation {
     public static void main(String[] args) {
+        printRow(4);
+    }
+
+    private static void printRow(int n) {
         int i = 1;
         while(i <= 6) {
-            System.out.printf("%4d", 2 * i);
+            System.out.printf("%4d", n * i);
             i = i + 1;
         }
         System.out.println();
