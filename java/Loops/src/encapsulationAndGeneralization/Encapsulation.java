0af17e8eff72e84c070fc33c3047fc862f70f141
guganems <g_nemsveridze@cu.edu.ge>
2019-07-04 12:55:00 +0400

    add encapsulation

---
+++ java/Loops/src/encapsulationAndGeneralization/Encapsulation.java
@@ -0,0 +1,12 @@
+package encapsulationAndGeneralization;
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i <= 6) {
+            System.out.printf("%4d", 2 * i);
+            i = i + 1;
+        }
+        System.out.println();
+    }
+}
