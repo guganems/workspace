71110317301704d6c8956e850e751ad2ae59f2f2
guganems <g_nemsveridze@cu.edu.ge>
2019-07-04 12:47:54 +0400

    Add log(2)

--- java/Loops/src/generatingTables/Table.java
+++ java/Loops/src/generatingTables/Table.java
@@ -5,7 +5,7 @@ public class Table {
         int i = 1;
         while (i < 10) {
             double x = i;
-            System.out.println(x + "  " + Math.log(x));
+            System.out.println(x + "  " + Math.log(x) / Math.log(2));
             i = i + 1;
         }
     }
