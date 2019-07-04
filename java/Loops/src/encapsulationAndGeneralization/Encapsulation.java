800dc645fc0aee9b0ade4c08d25b815c95519332
guganems <g_nemsveridze@cu.edu.ge>
2019-07-04 13:11:50 +0400

    More generalization

--- java/Loops/src/encapsulationAndGeneralization/Encapsulation.java
+++ java/Loops/src/encapsulationAndGeneralization/Encapsulation.java
@@ -1,14 +1,21 @@
 package encapsulationAndGeneralization;
 
+import java.util.Scanner;
+
 public class Encapsulation {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int rows = input.nextInt();
+        printTable(rows);
+    }
+
+    private static void printTable(int rows) {
         int i = 1;
-        while (i <= 6) {
+        while (i <= rows) {
             printRow(i);
             i = i + 1;
         }
     }
-
     private static void printRow(int n) {
         int i = 1;
         while(i <= 6) {
