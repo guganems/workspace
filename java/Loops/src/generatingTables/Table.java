86f8d37d540599cb1e0842df862a52402aeb28b3
guganems <g_nemsveridze@cu.edu.ge>
2019-07-04 12:50:44 +0400

    Change attitude

--- java/Loops/src/generatingTables/Table.java
+++ java/Loops/src/generatingTables/Table.java
@@ -2,11 +2,12 @@ package generatingTables;
 
 public class Table {
     public static void main(String[] args) {
+        final double LOG2 = Math.log(2);
         int i = 1;
-        while (i < 10) {
+        while (i < 100) {
             double x = i;
-            System.out.println(x + "  " + Math.log(x) / Math.log(2));
-            i = i + 1;
+            System.out.println(x + "  " + Math.log(x) / LOG2);
+            i = i * 2;
         }
     }
 }
