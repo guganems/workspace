608d61bba0820cf98eddc952b47f31d540a640d5
guganems <g_nemsveridze@cu.edu.ge>
2019-07-08 17:12:39 +0400

    Add exerc1

---
+++ java/StringsAndThings/src/command_lineArguments/Max.java
@@ -0,0 +1,10 @@
+package command_lineArguments;
+
+import java.util.Arrays;
+
+public class Max {
+
+    public static void mn(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
