bb2190ec8d84bc7f825237ea087018f2c1859d02
guganems <g_nemsveridze@cu.edu.ge>
2019-07-09 17:41:40 +0400

    Add

---
+++ java/Objects/src/objectsAndParameters/Object.java
@@ -0,0 +1,10 @@
+package objectsAndParameters;
+
+import java.awt.*;
+
+public class Object {
+    public static void main(String[] args) {
+        Point point = new Point(3, 4);
+        System.out.println(point);
+    }
+}
