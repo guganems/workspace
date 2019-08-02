c3104d1ae0a74f3bb1a9fa85d33fd13902036791
guganems <g_nemsveridze@cu.edu.ge>
2019-08-02 13:26:53 +0400

    moveRect

--- java/Objects/src/objectsAsReturnType/Objects.java
+++ java/Objects/src/objectsAsReturnType/Objects.java
@@ -6,5 +6,12 @@ public class Objects {
     public static void main(String[] args) {
         Rectangle box = new Rectangle(0, 0, 100, 200);
         System.out.println(box);
+        moveRect(box, 50, 100);
+        System.out.println(box);
+    }
+
+    public static void moveRect(Rectangle box, int dx, int dy) {
+        box.x = box.x + dx;
+        box.y = box.y + dy;
     }
 }
