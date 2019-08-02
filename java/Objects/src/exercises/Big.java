b0a6d68264ddb7a1e1b25814378b038120ba4b86
guganems <g_nemsveridze@cu.edu.ge>
2019-08-02 14:10:55 +0400

    bigint

--- java/Objects/src/exercises/Big.java
+++ java/Objects/src/exercises/Big.java
@@ -1,15 +1,25 @@
 package exercises;
 
+import java.math.BigInteger;
+
 public class Big {
     public static void main(String[] args) {
-        for (int i = 0; i <= 30; i++) {
+        for (int i = 0; i <= 3000; i++) {
             System.out.println(i + "    " + factorial(i));
         }
+
+        BigInteger small = BigInteger.valueOf(17);
+        BigInteger big = BigInteger.valueOf(1700000000);
+        BigInteger product = small.multiply(big);
+        BigInteger pow = small.pow(170000);
+        System.out.println(product);
+        System.out.println(pow);
+        System.out.println("END");
     }
-    private static int factorial(double n) {
-        int fact = 1;
+    private static BigInteger factorial(double n) {
+        BigInteger fact = BigInteger.valueOf(1);
         for (int i = 1; i <= n; i ++) {
-            fact *= i;
+            fact = fact.multiply(BigInteger.valueOf(i));
         }
         return fact;
     }
