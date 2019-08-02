f4360c7049674a3374fd98bec82714066659d8be
guganems <g_nemsveridze@cu.edu.ge>
2019-08-02 13:22:10 +0400

    Objects as return type

---
+++ java/Objects/src/objectsAsReturnType/Objects.java
@@ -0,0 +1,10 @@
+package objectsAsReturnType;
+
+import java.awt.Rectangle;
+
+public class Objects {
+    public static void main(String[] args) {
+        Rectangle box = new Rectangle(0, 0, 100, 200);
+        System.out.println(box);
+    }
+}
