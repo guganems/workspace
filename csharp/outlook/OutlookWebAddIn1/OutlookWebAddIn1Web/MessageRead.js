f66825116125076f5b1c53d012ff9833ed4d8657
guganems <g_nemsveridze@cu.edu.ge>
2019-08-05 17:13:09 +0400

    Why i didn't use twodio?

---
+++ csharp/outlook/OutlookWebAddIn1/OutlookWebAddIn1Web/MessageRead.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+(function () {
+    Office.onReady(function () {
+        $(document).ready(function () {
+            loadItemProps(Office.context.mailbox.item);
+        });
+    });
+
+    function loadItemProps(item) {
+        $("#item-id").text(item.itemId);
+        $("#item-subject").text(item.subject);
+        $("#item-internetMessageId").text(item.internetMessageId);
+        $("#item-from").html(item.from.displayName + " &lt; " + item.from.emailAdress + "&gt;");
+    }
+});
