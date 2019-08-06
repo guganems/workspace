b4717793c1f96f7dd564437af36a2d9854f4bc45
guganems <g_nemsveridze@cu.edu.ge>
2019-08-06 15:02:21 +0400

    classes

---
+++ java/Classes/src/displayingObjects/Object.java
@@ -0,0 +1,8 @@
+package displayingObjects;
+
+public class Object {
+    public static void main(String[] args) {
+        Time time = new Time(11, 59, 59.9);
+        System.out.println(time);
+    }
+}
