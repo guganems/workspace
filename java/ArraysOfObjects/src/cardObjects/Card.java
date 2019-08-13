972b3f1578a96632ff45cb96ce9219b76fb07379
guganems <g_nemsveridze@cu.edu.ge>
2019-08-13 14:32:13 +0400

    Immutable cards

--- java/ArraysOfObjects/src/cardObjects/Card.java
+++ java/ArraysOfObjects/src/cardObjects/Card.java
@@ -1,8 +1,8 @@
 package cardObjects;
 
 public class Card {
-    private int rank;
-    private int suit;
+    private final int rank;
+    private final int suit;
     public static final String[] RANKS = {null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
     public static final String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
 
