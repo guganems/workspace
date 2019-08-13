2b58f6c6a9965cd96c097b8c786bcfd101032197
guganems <g_nemsveridze@cu.edu.ge>
2019-08-13 14:19:44 +0400

    toString

--- java/ArraysOfObjects/src/cardObjects/Card.java
+++ java/ArraysOfObjects/src/cardObjects/Card.java
@@ -8,4 +8,11 @@ public class Card {
         this.rank = rank;
         this.suit = suit;
     }
+
+    public String toString() {
+        String[] ranks = {null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "Jack", "Queen", "King"};
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        return ranks[this.rank] + " of " + suits[this.suit];
+    }
 }
---
+++ java/ArraysOfObjects/src/cardObjects/Main.java
@@ -0,0 +1,8 @@
+package cardObjects;
+
+public class Main {
+    public static void main(String[] args) {
+        Card card = new Card(11, 1);
+        System.out.println(card);
+    }
+}
