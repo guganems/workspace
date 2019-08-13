64f5a97cb1b0d42828eb1e6cef58ac31e143cfde
guganems <g_nemsveridze@cu.edu.ge>
2019-08-13 14:06:12 +0400

    Add Card

---
+++ java/ArraysOfObjects/src/cardObjects/Card.java
@@ -0,0 +1,11 @@
+package cardObjects;
+
+public class Card {
+    private int rank;
+    private int suit;
+
+    public Card (int rank, int suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+}
