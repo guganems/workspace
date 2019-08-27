36c6e520073d0ba99eeb978a77b5a8ba89a6515b
guganems <g_nemsveridze@cu.edu.ge>
2019-08-27 14:42:52 +0400

    asd

---
+++ csharp/WebServices/jstranslator/JSONTranslator/JSONTranslator/WebService1.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+
+namespace JSONTranslator
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        public static String Translate(String word)
+        {
+            return Encoding.UTF8.GetString(Encoding.Default.GetBytes(word));
+        }
+
+        [WebMethod]
+        public string HelloWorld(String word)
+        {
+            return Translate(word);
+        }
+    }
+}
